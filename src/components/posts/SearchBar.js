ae2e8f5fdea73636a521907d2bc36089fd19689f
Sanjay <sanjaylachat@gmail.com>
2017-12-10 02:42:28 +0530

    SearchBar added

---
+++ src/components/posts/SearchBar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Avatar from 'material-ui/Avatar'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+const SearchBar = () => (
+  <MuiThemeProvider>
+    <div style={{ width: '600px', height: '60px', backgroundColor: '#E8F5FD' }}>
+      <Avatar 
+        src="imgs/profile.jpg"
+        size={35}
+        style={{
+          marginLeft: '27px',
+          marginTop: '11px'
+        }}
+      />
+      <input 
+        type="text"
+        style={{
+          border: '0px', 
+          borderRadius: '8px',
+          position: 'relative', 
+          left: '15px',top: '-11px', 
+          paddingRight: '320px', 
+          paddingTop: '9px',
+          paddingBottom: '9px'
+        }} 
+      />
+    </div>
+  </MuiThemeProvider>
+)
+
+export default SearchBar
