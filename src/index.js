f929298c9121d4791715768961cd66b586b971d3
Sanjay <sanjaylachat@gmail.com>
2017-12-11 05:32:44 +0530

    404 Page Added !

--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
 //Importing Packages For ReactJS
 import React from 'react'
 import {render} from 'react-snapshot'
-import { Route, BrowserRouter } from 'react-router-dom'
+import { Route, Switch, BrowserRouter } from 'react-router-dom'
 import 'normalize-css'
 import registerServiceWorker from './registerServiceWorker'
 //Additional Imports 
 import App from './App'
 import ResultsPage from './Results'
 
+const nomatch = () => (
+  <div>
+    <center>
+      <b style={{fontSize: "50px"}}>404 Not Found!</b>
+    </center>
+  </div>
+)
 const Router = () => (
   <BrowserRouter>
-    <div>
+    <Switch>
       <Route path="/" component={App} exact={true} />
       <Route path="/search" component={ResultsPage} />
-    </div>
+      <Route component={nomatch} />
+    </Switch>
   </BrowserRouter>
 )
 //Render Elements from React
+
 render(<Router />, document.getElementById('root'))
 registerServiceWorker()
