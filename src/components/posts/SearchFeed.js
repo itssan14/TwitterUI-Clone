3006dda4b2fd37d1f0c1e10faa10a91c83ffea4d
Sanjay <sanjaylachat@gmail.com>
2017-12-11 21:17:23 +0530

    Images for aadhar page added !

--- src/components/posts/SearchFeed.js
+++ src/components/posts/SearchFeed.js
@@ -16,21 +16,21 @@ export default class Posts extends Component {
                 />
 
                 <Post 
-                    UserName="The Indian Express" 
-                    Tag="@IndianExpress"
-                    Img="express"
-                    PostImg="news" 
-                    content="Govt. extends deadline to link Aadhar with PAN"
-                    time="Dec 7"
+                    UserName="Realty Myths" 
+                    Tag="@RealtyMyths"
+                    Img="aadh2prof"
+                    PostImg="aadh2" 
+                    content="All the Govt. decisions that mattered with respect to property, Aadhar and more"
+                    time="Dec 4"
                 />
 
                 <Post 
-                    UserName="The Indian Express" 
-                    Tag="@IndianExpress"
-                    Img="express"
-                    PostImg="news" 
-                    content="Govt. extends deadline to link Aadhar with PAN"
-                    time="Dec 7"
+                    UserName="Ankit" 
+                    Tag="@Ankitaker"
+                    Img="aadh3"
+                    PostImg="aadh3prof" 
+                    content="Hrithik Roshan' s Aadhar Card pic is way more beautiful than other stars Twitter DP😛"
+                    time="40m"
                 />      
             </div>
 
